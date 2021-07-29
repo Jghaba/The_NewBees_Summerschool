c2bac39756ac3ba9bb3ac899ae3d8ccfb1e6e7c1
Jghaba <87374194+Jghaba@users.noreply.github.com>
2021-07-29 16:04:39 +0300

    style and index

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,2 +1,23 @@
 <?php
-// Silence is golden.
+echo '<html>';
+echo '<head>';
+echo '<meta charset="utf-8">';
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '<title>The NewBees | Online Market</title>';
+echo '<meta name='description' content ="The NewBees | Online Market">';
+echo '<meta name="viewport" content="width=device-width,initial-scale=1">';
+echo '<link rel="stylesheet " href="style.css">';
+echo '</head>';
+echo '<body>';
+echo '<div class="wrapper">';
+echo '<section class="top">';
+echo '<div class="top_content">';
+echo '<p class="subtitle">Online Market </p>';
+echo '<h1 class="title">The NewBees </h1>';
+echo '</div>';
+echo '<div class="top_profile"></div>';
+echo '</section>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
+?>
