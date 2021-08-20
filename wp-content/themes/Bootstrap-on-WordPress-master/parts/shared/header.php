77693e5e0f19aeb354a872deb82f8c89590bb51c
Andrei Bratosin <SpaceBearOne@ProtonMail.com>
2021-08-20 16:01:51 +0300

    added notification menu to navbar

--- wp-content/themes/Bootstrap-on-WordPress-master/parts/shared/header.php
+++ wp-content/themes/Bootstrap-on-WordPress-master/parts/shared/header.php
@@ -1,3 +1,50 @@
+<style>
+				/* Dropdown Button */
+	.dropbtn {
+	background-color: #04AA6D;
+	color: white;
+	padding: 16px;
+	font-size: 16px;
+	border: none;
+	}
+
+	/* The container <div> - needed to position the dropdown content */
+	.dropdown {
+	position: relative;
+	display: inline-block;
+	}
+
+	/* Dropdown Content (Hidden by Default) */
+	.dropdown-content {
+	display: none;
+	position: absolute;
+	background-color: #f1f1f1;
+	min-width: 160px;
+	box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+	z-index: 1;
+	}
+
+	/* Links inside the dropdown */
+	.dropdown-content a {
+	color: black;
+	padding: 12px 16px;
+	text-decoration: none;
+	display: block;
+	}
+
+	/* Change color of dropdown links on hover */
+
+	/*.dropdown-content a:hover {background-color: #ddd;}
+
+	/* Show the dropdown menu on hover */
+	.dropdown:hover .dropdown-content {display: block;}
+
+	/* Change the background color of the dropdown button when the dropdown content is shown */
+	.dropdown:hover .dropbtn {background-color: #3e8e41;}  
+	.background_unseen{
+		background-color:blue;
+	}
+</style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container-fluid">
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#primaryNav" aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,6 +66,29 @@
 				'walker'         	=> new bootstrap_5_wp_nav_menu_walker()
 			));
 			?>
+			<?php
+			$notifications=get_posts([
+    		'post_type'=>'notification',
+			]); 
+			?>
+			<div class="dropdown">
+  			<button type="image" src="src="https://img.icons8.com/material-rounded/24/000000/appointment-reminders.png" class="dropbtn"></button>
+  			<div class="dropdown-content">
+				
+			<?php
+				foreach($notifications as &$notification){
+					if(get_field('seen', $notification->ID)==0){
+					echo('<div class="background_unseen" id="div'.$notification->ID.'">');
+					}else echo('<div id="div'.$notification->ID.'">');
+					echo "<h4 onmouseover='mark_as_seen(".$notification->ID.");refresh_element(".$notification->ID.")'>".$notification->post_title."</h4>";
+					echo $notification->post_content."</a>";
+					echo('</div>');
+				}
+			?>
+			</div>
+			</div>
+
+
 			<?php get_search_form(); ?>
 		</div>
 	</div>
@@ -39,3 +109,5 @@
   ?>
   <br/>
 </div> 
+
+
