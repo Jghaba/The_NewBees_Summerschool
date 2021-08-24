18e5509338e9f7473520c27271c90d94a771b4c1
oana-star <oana.podoiu13@gmail.com>
2021-08-24 11:35:29 +0300

    products page

--- wp-content/themes/Bootstrap-on-WordPress-master/page-products.php
+++ wp-content/themes/Bootstrap-on-WordPress-master/page-products.php
@@ -4,13 +4,17 @@
                 $args = array(
                   'post_type' => 'product',
                   'post_status' => 'publish',
+                  'paginate' => true,
                 )
               );
               foreach($products as &$product){
                 $wp_product= new WC_Product($product->ID);
-                echo "<li>";
-                echo '<img src="' . $wp_product->get_image() . '" alt="' . $product->post_title . '">';
-                echo "<li>";
+                echo 
+                '<li>
+                  <a href="' . get_permalink() . $product->post_title. '">
+                    <img src="' . $wp_product->get_image() . '" alt="' .$product->post_title . '">
+                  </a>
+                </li>';
               }
               wp_reset_postdata();?>
               </ul>
